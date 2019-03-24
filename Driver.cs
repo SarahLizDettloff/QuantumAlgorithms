03f68794cab07ab277a595da9b823d3032e1f216
Sarah Liz Dettloff <Sarah.Liz.Dettloff@gmail.com>
2019-03-24 01:24:58 -0400

    Add files via upload

---
+++ Driver.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+using Microsoft.Quantum.Simulation.Simulators.QCTraceSimulators;
+
+namespace Quantum.QuantumAlgorithms
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (var qsim = new QuantumSimulator())
+                {
+                    Result[] initials = new Result[] { Result.Zero, Result.One };
+                    foreach (Result initial in initials)
+                    {
+                        var res = QuantumAlgorithmsTest.Run(qsim, 1000, initial).Result;
+                        var (numZeros, numOnes, agree) = res;
+                        System.Console.WriteLine(
+                            $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agree,-4}");
+                    }
+                }
+                // This Estimator is used to estimate how many qubits or gates the program will use when executed on a quantum computer
+                var estimator = new ResourcesEstimator();
+                var traceSimCfg = new QCTraceSimulatorConfiguration();
+                traceSimCfg.useDistinctInputsChecker = true; 
+                QCTraceSimulator sim = new QCTraceSimulator(traceSimCfg);
+                QuantumAlgorithmsTest.Run(estimator, 1000, Result.Zero).Wait();
+
+                var data = estimator.Data;
+                Console.WriteLine($"");
+                Console.WriteLine($"QubitCliffords: {data.Rows.Find("QubitClifford")["Sum"]}");
+                Console.WriteLine($"Ts: {data.Rows.Find("T")["Sum"]}");
+                Console.WriteLine($"CNOTs: {data.Rows.Find("CNOT")["Sum"]}");
+
+                Console.WriteLine($"");
+                Console.WriteLine($"Trace Simulator: Distinct Inputs Checker");
+                var traceData = traceSimCfg.callStackDepthLimit;
+                System.Console.WriteLine(traceData.ToString());
+                System.Console.WriteLine(traceData.GetQubits());
+                Console.WriteLine($"Full estimator details:");
+                System.Console.WriteLine(estimator.ToTSV());
+
+                System.Console.WriteLine("Press any key to continue...");
+                Console.ReadKey();
+            }
+            catch (AggregateException e)
+            {
+                // Fetches inner exceptions
+                foreach (Exception inner in e.InnerExceptions)
+                {
+                    if (inner is ExecutionFailException failureException)
+                    {
+                        Console.WriteLine($" {failureException.Message}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Operations.qs
@@ -0,0 +1,66 @@
+﻿namespace Quantum.QuantumAlgorithms
+{
+    open Microsoft.Quantum.Primitive;
+    open Microsoft.Quantum.Canon;
+    
+    operation Set (desired: Result, q1: Qubit) : Unit 
+	{
+		let current = M(q1);
+		if (desired != current)
+			{
+				X(q1);
+			}
+    }
+
+	 operation QuantumAlgorithmsTest (count : Int, initial: Result) : (Int, Int, Int)
+    {
+		mutable numOnes = 0;
+        mutable agree = 0;
+        using (qubits = Qubit[2])
+        {
+            for (test in 1..count)
+            {
+                Set (initial, qubits[0]);
+                Set (Zero, qubits[1]);
+
+                H(qubits[0]);
+                CNOT(qubits[0], qubits[1]);
+                let res = M (qubits[0]);
+
+                if (M (qubits[1]) == res) 
+                {
+                    set agree = agree + 1;
+                }
+                if (res == One)
+                {
+                    set numOnes = numOnes + 1;
+                }
+            }
+            
+            Set(Zero, qubits[0]);
+            Set(Zero, qubits[1]);
+        }
+        return (count-numOnes, numOnes, agree);
+    }
+
+	operation Teleportation (source : Qubit, target : Qubit) : Unit {
+    body (...) {
+        using (ancillaRegister = Qubit[1]) {
+            let ancilla = ancillaRegister[0];
+
+            H(ancilla);
+            CNOT(ancilla, target);
+
+            CNOT(source, ancilla);
+            H(source);
+
+            AssertProb([PauliZ], [source], Zero, 0.5, "Outcomes must be equally likely", 1e-5);
+            AssertProb([PauliZ], [ancilla], Zero, 0.5, "Outcomes must be equally likely", 1e-5);
+
+            if (M(source) == One)  { Z(target); X(source); }
+            if (M(ancilla) == One) { X(target); X(ancilla); }
+        }
+    }
+	}
+	
+}
